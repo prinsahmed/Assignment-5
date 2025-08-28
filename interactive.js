f404b1b642349d0a99665ecfa70d1a38dc60f45e
prinsahmed <s.a.prince.offiziell@gmail.com>
2025-08-28 21:56:04 +0600

    made dynamic

---
+++ interactive.js
@@ -0,0 +1,74 @@
+// heart counter
+
+const heartElements = document.getElementsByClassName('heart-logo');
+ for(let ele of heartElements){
+    ele.addEventListener('click', function(){
+    
+        let heartCounter = parseInt(document.getElementById('hrt').innerText);
+        heartCounter++;
+       document.getElementById('hrt').innerText = heartCounter ;
+    
+});
+ }
+//  call function
+const callGenarated = document.getElementsByClassName('callname');
+
+
+ for(let i = 0; i < callGenarated.length; i++){
+    callGenarated[i].addEventListener('click', function(){
+        let getCredit = parseInt( document.getElementById('credit').innerText);
+        if(getCredit >= 20){
+            getCredit -= 20;
+            document.getElementById('credit').innerText = getCredit;
+            const Name = [  'National Emergency Services',
+                            'Police Helpline Services',
+                            'Fire Service Services',
+                            'Ambulance Service',
+                            'Women & Child Helpline',
+                            'Anti-Corruption Helpline',
+                            'Electricity Helpline',
+                            'Brac Helpline',
+                            'Bangladesh Railway Helpline'
+                         ]
+
+            const Number = [999, 999, 999, 1994999999, 109, 106, 16216, 16445,
+                163 
+            ]             
+
+         const div = document.createElement('div');
+            const h2 = document.createElement('h2');
+            h2.innerText = Name[i];
+            const h3 = document.createElement('h3');
+            h3.innerText = Number[i];
+            div.appendChild(h2);
+            div.appendChild(h3);
+            const classs = document.getElementById('call-list');
+            classs.appendChild(div);
+            div.classList.add('p-2', 'text-white', 'mt-3', 'rounded-xl', 'bg-[#74b9ff]')
+            h2.classList.add('text-lg')
+            
+                         alert(
+                            `Calling ${Name[i]} ${Number[i]}`
+                         )}
+        else{
+            alert('Insufficient Credit')
+        }
+    })
+ }
+//  clear
+document.getElementById('clearbtn').addEventListener('click', function(){
+    const div = document.getElementById('call-list');
+    div.innerText = '';
+    
+});
+// copy part
+const copyCounter = document.getElementsByClassName('copy');
+ for(let ele of copyCounter){
+    ele.addEventListener('click', function(){
+    
+        let copyCounter = parseInt(document.getElementById('copycntr').innerText);
+        copyCounter++;
+       document.getElementById('copycntr').innerText = copyCounter ;
+    
+});
+ }
